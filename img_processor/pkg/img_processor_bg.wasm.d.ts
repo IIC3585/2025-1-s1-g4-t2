66fb428d096dda6c57d79a56b65bed08528789cb
Amandasandov <amanda.sandoval@uc.cl>
2025-04-17 13:21:17 -0400

    Updates

---
+++ img_processor/pkg/img_processor_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const resize: (a: number, b: number, c: number, d: number) => [number, number];
+export const grayscale: (a: number, b: number) => [number, number];
+export const blur: (a: number, b: number, c: number) => [number, number];
+export const invert: (a: number, b: number) => [number, number];
+export const brighten: (a: number, b: number, c: number) => [number, number];
+export const flip_horizontal: (a: number, b: number) => [number, number];
+export const rotate90: (a: number, b: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
