6021cb30c7cdc1384646c08ba6ed77db01ae0012
Nicolás Eduardo Cañete Moena <65875467+Artaudd@users.noreply.github.com>
2025-04-18 20:41:21 -0400

    build: update url for merge to main repo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/T2-WebAvanzado-Fork/',
+  base: '/2025-1-s1-g4-t2/',
   plugins: [react()],
   assetsInclude: ['**/*.wasm'],
 });
